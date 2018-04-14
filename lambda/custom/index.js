8fb295ed770a5c1c038fc6e2fb1a874bdd0a5466
EndaMolloy <enda.molloy.1@gmail.com>
2018-04-14 21:11:55 +0100

    added check to see if lesson already saved to DB

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -17,7 +17,8 @@ const SKILL_NAME = 'Life Lessons';
 const HELP_MESSAGE = 'You can say give me a life lesson, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
-var todaysLesson = '';
+var todaysLessonTitle = '';
+var todaysLessonContent = '';
 
 exports.handler = function(event, context) {
     var alexa = Alexa.handler(event, context);
@@ -28,44 +29,45 @@ exports.handler = function(event, context) {
 
 const handlers = {
 
-
-
     'LaunchRequest': function () {
       //this.response.speak(welcomeOutput).listen(welcomeReprompt);
       this.emit('GetNewLessonIntent');
     },
     'GetNewLessonIntent': function () {
-      // axios.get(`https://spreadsheets.google.com/feeds/list/1eDComL5qWGUo_aC07-T-rIAzS-mPZ3ctxBkEJwbGZS0/od6/public/basic?alt=json`)
-      // .then(response => {
-      //   console.log(response.data.feed.entry[1].title.$t);
-      //
-      //   todaysLesson = response.data.feed.entry[1].title.$t;
-      //   this.response.speak(todaysLesson).shouldEndSession(false);
-      //   this.emit(':responseReady');
-      // })
-      // .catch(error => {
-      //   console.log(error);
-      // })
-      todaysLesson = 'hello mate';
-      this.response.speak(todaysLesson).shouldEndSession(false);
-      this.emit(':responseReady');
+      axios.get(`https://spreadsheets.google.com/feeds/list/1eDComL5qWGUo_aC07-T-rIAzS-mPZ3ctxBkEJwbGZS0/od6/public/basic?alt=json`)
+      .then(response => {
+        console.log(response.data.feed.entry[1].title.$t);
+
+        todaysLessonTitle = response.data.feed.entry[1].title.$t;
+        todaysLessonContent = response.data.feed.entry[1].content.$t;
+        // var speechOutput = todaysLessonTitle + "  " + todaysLessonContent;
+
+        this.response.speak(`Today\'s lesson is called <p>${todaysLessonTitle}</p><p>${todaysLessonContent}</p>`).shouldEndSession(false);
+        this.emit(':responseReady');
+
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      // todaysLesson = 'hello mate';
+      // this.response.speak(todaysLesson).shouldEndSession(false);
+      // this.emit(':responseReady');
     },
     'AddFavoriteLessonIntent': function(){
       const params = {
        TableName: favoriteLessonsTable,
        Item: {
-         "Title": 'The Wolf and his Shadow',
-         "lessonText": 'Hello mister wolf, I am your shadow'
+         "Title": todaysLessonTitle,
+         "lessonText": todaysLessonContent
        }
       };
 
-      // const checkIfLesson = {
-      //   TableName: favoriteLessonsTable,
-      //   Key: {
-      //     Title: 'The Wolf and his Shadow',
-      //     lessonText: 'Hello mister wolf, I am your shadow'
-      //   }
-      // }
+      const checkIfLesson = {
+        TableName: favoriteLessonsTable,
+        Key: {
+          'Title': todaysLessonTitle
+        }
+      }
 
       // dbPut(params).then(data => {
       //   console.log("PutItem succeeded: The Wolf Hahaha");
@@ -76,31 +78,38 @@ const handlers = {
       // })
       //  console.log('Attempting to add recipe', params);
       //
-      docClient.put(params).promise()
-      .then(data => {
-          console.log("data added successfully", data);
-          this.emit(':tell', 'The wolf and his shadow has been added to your favorites')
-      })
-      .catch(err => {
-          console.log(err);
-      });
+      // docClient.put(params).promise()
+      // .then(data => {
+      //     console.log("data added successfully", data);
+      //     this.emit(':tell', `${todaysLessonTitle} has been added to your favorites`);
+      // })
+      // .catch(err => {
+      //     console.log(err);
+      // });
 
-      //  docClient.get(checkIfLesson, function(err,data){
-      //    console.log('found lesson', data);
-       //
-      //    if(data.Item){
-      //      const errMessage = `${data.Item.Title} is already favorited`;
-      //      this.emit(':tell', errMessage);
-      //    }
-      //    else{
-      //      docClient.put(params, function(err,data) => {
-      //        if(data){
-      //          console.log("data added successfully", data);
-      //          this.emit(':tell', 'The wolf and his shadow has been added to your favorites')
-      //        }
-      //      });
-      //    }
-      //  })
+     docClient.get(checkIfLesson).promise()
+       .then(data => {
+         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+
+         if(data.Item){
+           console.log("Item already exists in DB");
+           this.emit(':tell', `${todaysLessonTitle} has already been added to your favorites`);
+         }
+         else {
+           docClient.put(params).promise()
+           .then(data => {
+               console.log("data added successfully", data);
+               this.emit(':tell', `${todaysLessonTitle} has been added to your favorites`);
+           })
+           .catch(err => {
+               console.log(err);
+           });
+         }
+       })
+       .catch(err => {
+         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+       })
+
       //  .then(data =>{
       //    console.log("data added successfully", data);
       //    this.emit(':tell', 'The wolf and his shadow has been added to your favorites')
@@ -110,10 +119,12 @@ const handlers = {
       //  })
     },
     'RepeatLessonIntent': function(){
-      this.response.speak(todaysLesson);
+      this.response.speak(`<p>${todaysLessonTitle}</p><p>${todaysLessonContent}</p>`);
       this.emit(':responseReady');
     },
-
+    'ListFavoritesIntent': function(){
+      
+    },
     'MyNameIsIntent': function () {
         this.emit('SayHelloName');
     },
